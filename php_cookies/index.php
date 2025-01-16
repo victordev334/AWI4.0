f2a8954ff13be42befbef9aa2816a4c61e917c4d
victordev334 <1723110220@utectulancingo.edu.mx>
2025-01-16 18:52:59 +0000

    reporte 1: sesiones y cookies

--- php_cookies/index.php
+++ php_cookies/index.php
@@ -1,40 +1,38 @@
 <?php
-session_start(); 
+session_start();
 
-$nivel = "usuario";  
-$user = "Felipe";     
-$pass = "1234";       
+// Credenciales predefinidas
+$_SESSION["user"] = "Dejah";
+$_SESSION["pass"] = "1234";
 
-if (!isset($_COOKIE[$nivel])) {
-    setcookie($nivel, $user,  time() + 3600,"/");  
-}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $usuario = $_POST["usuario"];
-    $contraseña = $_POST["contraseña"];
-    
-    
-    if ($usuario == $user && $contraseña == $pass) {
-        $_SESSION["nombre"] = $usuario;  
-        header("Location: bienvenido.php");  
-        exit;
+    if ($_POST['user'] === $_SESSION["user"] && $_POST['pass'] === $_SESSION["pass"]) {
+        $_SESSION["valido"] = true;
+        $_SESSION["status"] = activo;
+        header("Location: bienvenido.php");
+        exit();
     } else {
-        echo "Credenciales incorrectas. Por favor, intenta nuevamente.";
+        echo "Usuario o contraseña incorrectos.";
     }
 }
-
 ?>
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de Sesión</title>
+</head>
 <body>
-<form method="POST">
-    <p>Usuario:</p>
-    <input type="text" name="usuario" required>
-    <p>Contraseña:</p>
-    <input type="password" name="contraseña" required>
-    <br><br>
-    <button type="submit">iniciar</button>
-</form>
-
+    <form method="post" action="">
+        <label for="user">Usuario:</label>
+        <input type="text" name="user" required>
+        <br><br>
+        <label for="pass">Contraseña:</label>
+        <input type="password" name="pass" required>
+        <br><br>
+        <button type="submit">Iniciar Sesión</button>
+    </form>
 </body>
 </html>
